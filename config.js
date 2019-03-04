821d711248c1efe07a792cf5d46dd512a2d242b2
idsubar <nana11bot@gmail.com>
2019-03-04 06:39:58 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ exports.PREFIX = '!';
 
 exports.GOOGLE_API_KEY = 'AIzaSyAKVqhpqPw75grlzF6l0Zsv8h1_LdzLm7M';
 
-exports.owner = '286100702038851585';
+exports.owner = '530218480079863838';
 
-exports.CHID = '488818328568332293';
+exports.CHID = '530519946418192385';
 
 exports.LENGTH = 45; // الطول المسموح لأسم الاغنية قبل الإختصار
